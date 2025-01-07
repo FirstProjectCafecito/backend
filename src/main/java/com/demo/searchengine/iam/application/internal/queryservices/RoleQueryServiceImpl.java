baaf6e7d4ce3f2926a1615cba1c8ab0f62374f6e
Alejandro Espino <espinofloresa@gmail.com>
2025-01-07 18:54:37 -0500

    feat(iam): Role query service implementation methods

---
+++ src/main/java/com/demo/searchengine/iam/application/internal/queryservices/RoleQueryServiceImpl.java
@@ -0,0 +1,28 @@
+package com.demo.searchengine.iam.application.internal.queryservices;
+
+import com.demo.searchengine.iam.domain.model.entities.Role;
+import com.demo.searchengine.iam.domain.model.queries.GetAllRolesQuery;
+import com.demo.searchengine.iam.domain.model.queries.GetRoleByNameQuery;
+import com.demo.searchengine.iam.domain.services.RoleQueryService;
+import com.demo.searchengine.iam.insfrastructure.persistence.jpa.repositories.RoleRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class RoleQueryServiceImpl implements RoleQueryService {
+    private final RoleRepository roleRepository;
+
+    public RoleQueryServiceImpl(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public List<Role> handle(GetAllRolesQuery query) {
+        return roleRepository.findAll();
+    }
+
+    @Override
+    public Optional<Role> handle(GetRoleByNameQuery query) {
+        return roleRepository.findByName(query.name());
+    }
+}
